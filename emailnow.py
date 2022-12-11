df04570497de97403dcc4738f04183250a83e9f4
Spnetic-5 <sspowar_b19@el.vjti.ac.in>
2022-12-11 20:39:54 +0530

    feat: :rocket: Updated all files

--- emailnow.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-server = smtplib.SMTP('smtp.gmail.com',465)
-server.starttls()
-server.login("raspi.iot33","raspberryPi33")
-msg = 'test msg'
-server.sendmail("raspi.iot33","aaditya.newaskar@gmail.com",msg)
-server.quit()
